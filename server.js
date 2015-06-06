22f22491bc840347f6ffaf94c12ffceb079d53d1
Bertus Kruger <krugercreations@gmail.com>
2015-06-06 16:20:44 +1200

    Initial setup

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-// BASE SETUP
-// =============================================================================
+var I2C_addresses = [20,21];
+var port = 8080;
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+var MCP23017 = require('node-mcp23017');
+var express    = require('express');
+var app        = express();
+var router = express.Router();
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// more routes for our API will happen here
+router.get('/set', function(req, res) {
+    res.json({ message: 'hooray! welcome to our Set api!' });
+});
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+router.get('/get', function(req, res) {
+    console.log('Start'); //get GPIO B Pin 0 value
+
+    var mcp = new MCP23017({
+        address: 0x20, //all address pins pulled low
+        device: '/dev/i2c-1', // Model B
+        debug: false
+    });
+
+    console.log(mcp.getGpioBPinValue(0)); //get GPIO B Pin 0 value
+
+    res.json({ message: 'hooray! welcome to our Get api!' });
+});
+
+router.get('/mode', function(req, res) {
+    res.json({ message: 'hooray! welcome to our Mode api!' });
+});
+
 app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Magic happens at http://localhost:'+port+'/api');
